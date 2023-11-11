a2faabb71c396d1b50b19bea487e6df5129ed422
Abdi Surya Perdana <abdisury4@gmail.com>
2023-11-11 20:26:25 +0700

    add pop up tambah booking

---
+++ js/forminputbooking.js
@@ -0,0 +1,29 @@
+function openPopupForm() {
+    document.getElementById('popup-form').style.display = 'flex';
+  }
+  
+  function closePopupForm() {
+    document.getElementById('popup-form').style.display = 'none';
+  }
+  
+  function submitForm() {
+    // Ambil nilai dari inputan
+    var nama = document.getElementById('nama').value;
+    var noHp = document.getElementById('no-hp').value;
+    var jumlahOrang = document.getElementById('jumlah-orang').value;
+
+  
+    // Lakukan sesuatu dengan nilai inputan, misalnya kirim ke server atau tampilkan di console
+    console.log('Nama:', nama);
+    console.log('No HP:', noHp);
+    console.log('Jumlah Orang:', jumlahOrang);
+    console.log('Hari:', hari);
+    console.log('Kegiatan:', kegiatan);
+  
+    // Tutup pop-up form
+    closePopupForm();
+  }
+  
+  // Tambahan: Menambahkan event listener ke tombol untuk membuka pop-up form
+  document.querySelector('.custom-btn').addEventListener('click', openPopupForm);
+  
