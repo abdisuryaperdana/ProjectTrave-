be061e47a7e4fe3fd38c6e5336962e73528ec730
dastinaryo5@gmail.com <dastinaryo2@gmail.com>
2023-11-14 00:14:13 +0700

    js asu

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,3 @@ jQuery(document).ready(function() {
         fade: 750       // Fade speed in milliseconds
     });
 });
-
-// custom-script.js
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var divElement = document.getElementById('custom-background');
-
-//     // Set background image directly using style property
-//     divElement.style.backgroundImage = "url('images/backroundsumut.jpg')";
-//     divElement.style.backgroundSize = "cover"; // Adjust as needed
-//     divElement.style.backgroundRepeat = "no-repeat"; // Adjust as needed
-//     divElement.style.backgroundPosition = "center"; // Adjust as needed
-// });
